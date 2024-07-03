9cca3a1ee82e5675ed2c227b645d3e407c9f2b2b
piet-de-leeuw <pietdeleeuw156@gmail.com>
2024-07-03 17:19:46 +0200

    Added Qualify_For_Car_Insurance

---
+++ Basic_C#_Programs/Qualify_For_Car_Insurance/Qualify_For_Car_Insurance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Qualify_For_Car_Insurance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Please enter your age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            bool oldEnough = age > 15;
+
+            Console.WriteLine("\nHave you ever had a DUI?");
+            Console.WriteLine("please enter \"true\" or \"false\"");
+            bool hasDUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("\nHow many speeding tickets do you have?");
+            int AmountSpeedingTickets = Convert.ToInt32(Console.ReadLine());
+            bool lowAmoundSpeedingTicket = AmountSpeedingTickets <= 3;
+
+            bool isQualified = oldEnough && !hasDUI && lowAmoundSpeedingTicket;
+            Console.WriteLine("\nQualify's for a car insurance: " + isQualified);
+
+            Console.Read();
+        }
+    }
+}
