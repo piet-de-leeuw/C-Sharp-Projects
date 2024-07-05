19e62c6df544863a1f02d258ed855d772e130025
piet-de-leeuw <pietdeleeuw156@gmail.com>
2024-07-05 15:38:10 +0200

    Added Functions_Math

---
+++ Basic_C#_Programs/Functions_Math/Functions_Math/My_Functions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions_Math
+{
+    public static class My_Functions
+    {
+        public static int PlusFive(int number)
+        {
+            number += 5;
+            return number;
+        }
+
+        public static int MultiplyByTen(int number)
+        {
+            number *= 10;
+            return number;
+        }
+
+        public static int DivideByFive(int number)
+        {
+            number /= 5;
+            return number;
+        }
+
+
+    }
+}
---
+++ Basic_C#_Programs/Functions_Math/Functions_Math/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions_Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a whole number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            number = My_Functions.PlusFive(number);
+            Console.WriteLine(number);
+            number = My_Functions.MultiplyByTen(number);
+            Console.WriteLine(number);
+            number = My_Functions.DivideByFive(number);
+            Console.WriteLine(number);
+
+            Console.ReadLine();
+        }
+    }
+}
