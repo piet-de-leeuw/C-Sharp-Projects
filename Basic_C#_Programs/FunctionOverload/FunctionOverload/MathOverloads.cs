dd7ab40404e0524b6616b30dd8655ddedaf96b1e
piet-de-leeuw <pietdeleeuw156@gmail.com>
2024-07-05 17:12:05 +0200

    Added FunctionOverload

---
+++ Basic_C#_Programs/FunctionOverload/FunctionOverload/MathOverloads.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionOverload
+{
+    public class MathOverloads
+    {
+        //Overload 1
+        public int Plus(int numInt)
+        {
+            numInt += 100;
+            return numInt;
+        }
+
+        //Overload 2
+        public int Plus(decimal numDecimal)
+        {
+           int numInt;
+           numInt = Convert.ToInt32(numDecimal += 43.50m);
+           return numInt;
+        }
+
+        //Overload 3
+        public int Plus(string stringNumber)
+        {
+            try
+            {
+                int numInt;
+                numInt = Convert.ToInt32(stringNumber) + 500;
+                return numInt;
+            }
+            catch(FormatException ex)
+            {
+                int numInt = 1;
+                Console.WriteLine("Can't Convert string {0} into int, returned numInt as 1" );
+                Console.ReadLine();
+                return numInt;
+
+            }
+
+        }
+
+
+    }
+}
---
+++ Basic_C#_Programs/FunctionOverload/FunctionOverload/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numInt;
+
+            MathOverloads overloads = new MathOverloads();
+
+            //Using a interger input. Using overload 1;
+            numInt = overloads.Plus(50);
+            Console.WriteLine(numInt);
+
+            //Using a decimal input. Using overload 2;
+            numInt = overloads.Plus(42.55504m);
+            Console.WriteLine(numInt);
+
+            //Using a string input. Throws a error if the string cant be converted. Using overload 3;
+            numInt = overloads.Plus("200");
+            Console.WriteLine(numInt);
+
+
+            Console.ReadLine();
+        }
+    }
+}
