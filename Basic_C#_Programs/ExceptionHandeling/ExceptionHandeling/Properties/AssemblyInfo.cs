77d809447037256b817deb92e65f98443618f7bd
piet-de-leeuw <pietdeleeuw156@gmail.com>
2024-07-05 09:34:54 +0200

    added ExceptionHandeling

--- Basic_C#_Programs/ExceptionHandeling/ExceptionHandeling/Properties/AssemblyInfo.cs
+++ Basic_C#_Programs/ExceptionHandeling/ExceptionHandeling/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExeptionHandeling")]
+[assembly: AssemblyTitle("ExceptionHandeling")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExeptionHandeling")]
+[assembly: AssemblyProduct("ExceptionHandeling")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("49aa8296-35d0-4156-9320-87728a15ed6e")]
+[assembly: Guid("849f51d5-843c-4caf-a22a-dced42849401")]
 
 // Version information for an assembly consists of the following four values:
 //
