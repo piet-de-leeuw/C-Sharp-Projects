3f01ce572b505729a9628a88d50f4ab75a26e388
piet-de-leeuw <pietdeleeuw156@gmail.com>
2024-07-06 16:11:32 +0200

    Addet Inherit_Class

---
+++ Basic_C#_Programs/Inherit_Class/Inherit_Class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inherit_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee() { Id = 1001, FirstName = "Sample", LastName = "Student" };
+
+            //Call Superfunction 
+            employee.SayName();
+            Console.WriteLine(employee.Id);
+            Console.ReadLine();
+        }
+    }
+}
