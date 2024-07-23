57730e4d2b9edc70af20fbae98fc5383f3929bd7
piet-de-leeuw <pietdeleeuw156@gmail.com>
2024-07-23 13:16:43 +0200

    Implemented a generic class and a generic function to store differend types of data, in the property Things, in every new instance of class employee. and print it to the screen whit the generic function.

---
+++ Basic_C#_Programs/Generic_Class/Generic_Class/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Class
+ {   
+    public class Employee<T>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<T> Things { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/Generic_Class/Generic_Class/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee_1 = new Employee<string>();
+            Employee<int> employee_2 = new Employee<int>();
+
+            employee_1.Things = new List<string>() { "apple", "laptop", "backpack" };
+            employee_2.Things = new List<int>() { 1, 5, 5, 7 };
+
+            WriteThings(employee_1);
+            WriteThings(employee_2);
+
+            Console.ReadLine();
+
+            void WriteThings<T>(Employee<T> employee)
+            {
+
+                foreach(T thing in employee.Things)
+                {
+                    Console.WriteLine(thing);
+                }
+                
+            }
+
+        }
+    }
+}
