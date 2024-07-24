dfc582bf57b99f5170f45cd21f524cecd2853872
piet-de-leeuw <pietdeleeuw156@gmail.com>
2024-07-24 09:10:05 +0200

    Implemented an app that parses user input to a enum DayOfTheWeek

---
+++ Basic_C#_Programs/ParseEnums/ParseEnums/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please entter the currend week day...");
+                DaysOfTheWeek currendDay = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), Console.ReadLine());
+                Console.WriteLine(currendDay);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day.");
+            }
+            Console.ReadLine();
+            
+            
+        }
+        
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
