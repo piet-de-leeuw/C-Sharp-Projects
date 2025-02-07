1139a3c90b0c5ca8fb6c95b6cc767e85c67615ee
piet-de-leeuw <pietdeleeuw156@gmail.com>
2025-02-07 18:32:50 +0100

    Added WPFExercise

---
+++ WPF_Applications/WPFExercise/WPFExercise/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFExercise
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ WPF_Applications/WPFExercise/WPFExercise/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFExercise
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTodoButton_Click(object sender, RoutedEventArgs e)
+        {
+            string todoText = TextToWrite.Text;
+
+
+            if(!string.IsNullOrEmpty(todoText))
+            {
+                TextBlock newTodo = new TextBlock
+                {
+                    Text = "- " + todoText,
+                    Margin = new Thickness(10),
+                };
+
+
+                TodoStack.Children.Add(newTodo);
+
+                TextToWrite.Clear();
+            }
+
+        }
+
+        private void RemoveTodoButton_Click(Object sender, RoutedEventArgs e)
+        {
+            bool isInt = int.TryParse(NrToRemove.Text, out int nr);
+            bool isOutOfRange = nr >= TodoStack.Children.Count;
+
+            if(isInt && nr != 0 && !isOutOfRange)
+            {
+                TodoStack.Children.RemoveAt(nr);
+            }
+        }
+    }
+}
